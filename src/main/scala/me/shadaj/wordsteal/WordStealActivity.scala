cc60f4d16453db389c6c1e0103bd10fc89fa4ede
shadaj <shadaj>
2013-05-25 14:39:43 -0700

    Works with sbt

--- src/main/scala/me/shadaj/wordsteal/WordStealActivity.scala
+++ src/main/scala/me/shadaj/wordsteal/WordStealActivity.scala
@@ -9,29 +9,38 @@ import android.widget.TextView
 import scala.io.Source
 import android.view.View.OnKeyListener
 import android.view.KeyEvent
+import android.widget.Button
+import android.util.Log
+import android.text.method.KeyListener
+import android.text.Editable
+import android.text.TextWatcher
 
-class WordStealActivity extends Activity {
-  var currentPoints = 0
+class WordStealActivity extends TypedActivity {
+  lazy val charactersDisplay = findView(TR.characterDisplay)
+  lazy val input = findView(TR.input)
+  lazy val response = findView(TR.response)
+  lazy val checkButton = findView(TR.checkButton)
 
-  var words: List[String] = null
+  lazy val assets = getAssets()
+  lazy val en_US = Source.fromInputStream(assets.open("en_US.dic"))
+  lazy val words: Set[String] = en_US.getLines.toSet
 
   val A = 'a'.toInt
   val Z = 'z'.toInt
   val alphabetSize = Z - A + 1
 
-  def randomChar = ((math.random * alphabetSize) + A).toInt.toChar
+  def randomChar = ((math.random * alphabetSize) + A).toChar
 
-  lazy val charactersDisplay = findViewById(R.id.characterDisplay).asInstanceOf[TextView]
-  lazy val input = findViewById(R.id.input).asInstanceOf[EditText]
-  lazy val response = findViewById(R.id.response).asInstanceOf[TextView]
+
   var currentStart = randomChar
   var currentEnd = randomChar
+  var currentPoints = 0
 
   def newLetters {
     currentStart = randomChar
     currentEnd = randomChar
     if (words.count(s => s.head.toLower == currentStart && s.last.toLower == currentEnd) >= 100) {
-      charactersDisplay.setText(currentStart + " " + currentEnd)
+      charactersDisplay.setText(currentStart + "..." + currentEnd)
     } else {
       newLetters
     }
@@ -40,23 +49,38 @@ class WordStealActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    val assets = getAssets()
-    val en_US = Source.fromInputStream(assets.open("en_US.dic"))
-
-    words = en_US.getLines.toList
 
     newLetters
     
-    input.setOnKeyListener {new OnKeyListener () {
-      def onKey(v: View, keyCode: Int, event: KeyEvent) = {
-        if (keyCode == KeyEvent.KEYCODE_ENTER) {
-          checkWord(v)
-          true
+    val inputWatcher = new TextWatcher() {
+      def afterTextChanged(s: Editable) {}
+      def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+      def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        val inputWord = s.toString()
+        if (inputWord.isEmpty) {
+          checkButton.setEnabled(false)
+        } else if (inputWord.head.toLower == currentStart && inputWord.last.toLower == currentEnd) {
+          checkButton.setEnabled(true)
         } else {
-          false
+          checkButton.setEnabled(false)
         }
       }
-    }}
+    }
+
+    input.addTextChangedListener(inputWatcher)
+    
+    input.setOnKeyListener {
+      new OnKeyListener() {
+        def onKey(v: View, keyCode: Int, event: KeyEvent) = {
+          if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            checkWord(v)
+            true
+          } else {
+            false
+          }
+        }
+      }
+    }
   }
 
   def checkWord(view: View) {
@@ -73,7 +97,7 @@ class WordStealActivity extends Activity {
 
       response.setText(toSet)
     }
-    
+
     newLetters
   }
 }
