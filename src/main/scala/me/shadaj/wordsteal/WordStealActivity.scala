65a3afebd74dd7236521b9b89195d4d319810293
shadaj <shadaj>
2013-08-13 17:58:55 -0700

    Use preprocessed dictionary and have loading page

--- src/main/scala/me/shadaj/wordsteal/WordStealActivity.scala
+++ src/main/scala/me/shadaj/wordsteal/WordStealActivity.scala
@@ -1,18 +1,21 @@
 package me.shadaj.wordsteal
 
-import scala.annotation.tailrec
 import scala.io.Source
 
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
 import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
 import android.view.KeyEvent
 import android.view.View
 import android.view.View.OnKeyListener
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 
 class WordStealActivity extends Activity {
@@ -22,40 +25,70 @@ class WordStealActivity extends Activity {
   def checkButton = findViewById(R.id.checkButton).asInstanceOf[Button]
   def points = findViewById(R.id.points).asInstanceOf[TextView]
   def livesWidget = findViewById(R.id.lives).asInstanceOf[TextView]
+  def loadingProgress = findViewById(R.id.loadingProgress).asInstanceOf[ProgressBar]
+  def highScores = findViewById(R.id.highScores).asInstanceOf[TextView]
 
-  lazy val assets = getAssets()
-  lazy val en_US = Source.fromInputStream(assets.open("en_US.dic"))
-  lazy val words = en_US.getLines.map(_.toLowerCase).toSet
+  val PERCENTAGE_TO_LOAD = 0.05
+
+  val processedWords = collection.mutable.ArrayBuffer[(String, Set[String])]()
+
+  def processWords = {
+    var showingLoadingScreen = true
+    var linesSoFar = 0
+
+    val assets = getAssets
+
+    val dict = Source.fromInputStream(assets.open("web2.wordDic"))
+    val lines = dict.getLines()
+    val NUMBER_OF_LINES = lines.next.toInt
+
+    val updateProgressBar = new Runnable {
+      override def run {
+        if (showingLoadingScreen) {
+          loadingProgress.setProgress(((linesSoFar / (NUMBER_OF_LINES * PERCENTAGE_TO_LOAD)) * 100).toInt)
+        }
+      }
+    }
+
+    dict.getLines.foreach { s =>
+      val split = s.split(' ')
+      processedWords += ((split(0), split.tail.toSet))
+      if (linesSoFar / (NUMBER_OF_LINES * PERCENTAGE_TO_LOAD) >= 1 && showingLoadingScreen) {
+        showingLoadingScreen = false
+        val startGameRunnable = new Runnable {
+          override def run() {
+            startGame
+          }
+        }
+        runOnUiThread(startGameRunnable)
+      } else {
+        runOnUiThread(updateProgressBar)
+        linesSoFar += 1
+      }
+    }
+    dict.close
+  }
 
   val A = 'a'.toInt
   val Z = 'z'.toInt
   val alphabetSize = Z - A + 1
 
-  def randomChar = ((math.random * alphabetSize) + A).toChar
-
-  var currentStart = randomChar
-  var currentEnd = randomChar
   var currentPoints = 0
 
-  var lives = 3
+  var lives = 5
 
-  @tailrec
-  private def newLetters {
-    currentStart = randomChar
-    currentEnd = randomChar
-    if (words.count(s => s.head == currentStart && s.last == currentEnd) >= 100) {
-      charactersDisplay.setText(s"$currentStart...$currentEnd")
-    } else {
-      newLetters
-    }
+  var index = 0
+
+  def currentStart = processedWords(index)._1.head
+  def currentEnd = processedWords(index)._1.last
+
+  def newLetters {
+    index = (math.random * processedWords.size).toInt
+    val string = processedWords(index)._1
+    charactersDisplay.setText(currentStart + "..." + currentEnd)
   }
 
-  override def onCreate(savedInstanceState: Bundle) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.game)
-
-    newLetters
-
+  def addInputWatchers {
     val inputWatcher = new TextWatcher() {
       def afterTextChanged(s: Editable) {}
       def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
@@ -85,14 +118,52 @@ class WordStealActivity extends Activity {
         }
       }
     }
+  }
 
-    en_US.close
+  def startGame {
+    setContentView(R.layout.game)
+    newLetters
+    addInputWatchers
+  }
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.loading)
+
+    val loadThread = new Thread {
+      override def run() {
+        processWords
+      }
+    }
+
+    loadThread.start()
+  }
+
+  def gameOver {
+    val pref = getPreferences(Context.MODE_PRIVATE)
+    val previousScores = pref.getString("highscores", "-1").split(" ").toList.map(_.toInt).filter(_ >= 0)
+    val newScores = (currentPoints :: previousScores).sorted.reverse
+    val editor = pref.edit()
+    editor.putString("highscores", newScores.take(10).mkString(" "))
+    editor.commit()
+
+    val thisGameIndex = newScores.indexOf(currentPoints)
+    val thisGameText = "THIS GAME: " + newScores(thisGameIndex)
+    val thisGameDisplay = newScores.map(_.toString).updated(thisGameIndex, thisGameText).mkString("\n")
+
+    val thisGameColored = new SpannableStringBuilder(thisGameDisplay)
+    val thisGameStart = thisGameDisplay.indexOf('T')
+    val thisGameEnd = thisGameStart + thisGameText.length
+    thisGameColored.setSpan(new ForegroundColorSpan(android.graphics.Color.GREEN), thisGameStart, thisGameEnd, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+
+    setContentView(R.layout.gameover)
+    highScores.setText(thisGameColored)
   }
 
   def checkWord(view: View) {
-    val inputWord = input.getText().toString().toLowerCase
+    val inputWord = input.getText.toString.toLowerCase
     if (!(inputWord.length <= 1)) {
-      val correct = words.contains(inputWord)
+      val correct = processedWords(index)._2.contains(inputWord.tail.init)
       val responseText = correct match {
         case true => "Correct!"
         case false => "Incorrect :("
@@ -103,50 +174,36 @@ class WordStealActivity extends Activity {
       if (correct) {
         currentPoints += inputWord.length() * 100
         points.setText("Points: " + currentPoints)
+        response.setTextColor(android.graphics.Color.GREEN)
       }
 
       response.setText(responseText)
-
       newLetters
 
       if (!correct) {
+        response.setTextColor(android.graphics.Color.RED)
         lives -= 1
 
-        livesWidget.setText("Lives: " + lives)
-        
         if (lives == 0) {
-          val pref = getPreferences(Context.MODE_PRIVATE)
-          val previousScores = pref.getString("highscores", "-1").split(" ").toList.map(_.toInt).filter(_ >= 0)
-          val newScores = (currentPoints :: previousScores).sorted.reverse
-          val editor = pref.edit()
-          editor.putString("highscores", newScores.take(10).mkString(" "))
-          editor.commit()
-          val yourIndex = newScores.indexOf(currentPoints)
-          val toDisplay = newScores.map(_.toString).updated(yourIndex, "THIS GAME: " + newScores(yourIndex)).mkString("\n")
-          setContentView(R.layout.gameover)
-          findViewById(R.id.highScores).asInstanceOf[TextView].setText(toDisplay)
+          gameOver
+        } else {
+          livesWidget.setText("Lives: " + lives)
         }
       }
     }
   }
 
   def skipWord(view: View) {
-    newLetters
-    response.setText("Skipped")
     lives -= 1
-    livesWidget.setText("Lives: " + lives)
-    
+
     if (lives == 0) {
-      val pref = getPreferences(Context.MODE_PRIVATE)
-      val previousScores = pref.getString("highscores", "-1").split(" ").toList.map(_.toInt).filter(_ >= 0)
-      val newScores = (currentPoints :: previousScores).sorted.reverse
-      val editor = pref.edit()
-      editor.putString("highscores", newScores.take(10).mkString(" "))
-      editor.commit()
-      val yourIndex = newScores.indexOf(currentPoints)
-      val toDisplay = newScores.map(_.toString).updated(yourIndex, "THIS GAME: " + newScores(yourIndex)).mkString("\n")
-      setContentView(R.layout.gameover)
-      findViewById(R.id.highScores).asInstanceOf[TextView].setText(toDisplay)
+      gameOver
+    } else {
+      newLetters
+      response.setTextColor(android.graphics.Color.YELLOW)
+      response.setText("Skipped")
+      livesWidget.setText("Lives: " + lives)
+      input.setText("")
     }
   }
 
@@ -156,34 +213,6 @@ class WordStealActivity extends Activity {
     setContentView(R.layout.game)
     newLetters
 
-    val inputWatcher = new TextWatcher() {
-      def afterTextChanged(s: Editable) {}
-      def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-      def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        val inputWord = s.toString().toLowerCase
-        if (inputWord.length == 0) {
-          checkButton.setEnabled(false)
-        } else if (inputWord.head == currentStart && inputWord.last == currentEnd) {
-          checkButton.setEnabled(true)
-        } else {
-          checkButton.setEnabled(false)
-        }
-      }
-    }
-
-    input.addTextChangedListener(inputWatcher)
-
-    input.setOnKeyListener {
-      new OnKeyListener() {
-        def onKey(v: View, keyCode: Int, event: KeyEvent) = {
-          if (keyCode == KeyEvent.KEYCODE_ENTER) {
-            checkWord(v)
-            true
-          } else {
-            false
-          }
-        }
-      }
-    }
+    addInputWatchers
   }
 }
